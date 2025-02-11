b3e49b5a13ff77d9e2c7be2d818ca10ec7530389
Amanda1703 <144665@student.dcterra.nl>
2025-02-11 13:09:04 +0100

    php opdracht
    database toevoeging

---
+++ app/database.php
@@ -0,0 +1,24 @@
+<?php
+
+    class DBConnect {
+
+        private $servername = "mysql";
+        private $username = "root";
+        private $password = "root";
+        private $dbname = "userlogin";
+        public $conn;
+
+        public function __construct() {
+            try {
+                $this->conn = new PDO("mysql:host={$this->servername};dbname={$this->dbname}", $this->username, $this->password);
+                // set the PDO error mode to exception
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo "Connected successfully";
+              } catch(PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+              }
+        }
+    }
+
+
+?>
